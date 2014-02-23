94d1c3037abe9d7cd0aad17681b2fa26d3c477fb
Jason Frame <jason@onehackoranother.com>
2014-02-23 16:35:46 +0000

    hex/binary literals, allow underscores in numeric literals

--- test.js
+++ test.js
@@ -8,6 +8,9 @@ console.log(inspect(xson.parse(source, {
 	constructors: {
 		Foo: function(props) {
 			return { type: 'foo', props: props }
+		},
+		Person: function(props) {
+			return { type: 'person', props: props }
 		}
 	}
 }), {depth: null, colors: true}));
