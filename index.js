ecb6cafd7cd95ba68e17eadd1b0f547292c779db
Jason Frame <jason@onehackoranother.com>
2014-02-23 16:16:07 +0000

    vectors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var parser = require('./lib/parser');
 
+function identity(x) { return x; }
+
 module.exports = {
 	parse: function(source, options) {
 		options = options || {};
 
 		var ctors = options.constructors || {};
 		
-		if (!ctors.money) {
-			ctors.money = function(props) {
-				return props;
-			}
-		}
+		if (!ctors.money)	ctors.money = identity;
+		if (!ctors.vec2)	ctors.vec2 	= identity;
+		if (!ctors.vec3)	ctors.vec3 	= identity;
 
 		return parser.parse(source, {
 			constructors	: ctors
