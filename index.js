6d4ffc359c97dba0b6302c1056a9e4b25c288b2d
Jason Frame <jason@onehackoranother.com>
2014-03-07 22:19:29 +0000

    explicit constructors for builtin object types

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-var parser = require('./lib/parser');
+var parser  = require('./lib/parser'),
+    builtin = require('./lib/builtin');
 
 function identity(x) { return x; }
 
 module.exports = {
-	parse: function(source, options) {
-		options = options || {};
+    parse: function(source, options) {
+        options = options || {};
 
-		var ctors = options.constructors || {};
-		
-		if (!ctors.money)	ctors.money = identity;
-		if (!ctors.vec2)	ctors.vec2 	= identity;
-		if (!ctors.vec3)	ctors.vec3 	= identity;
+        var ctors = options.constructors || {};
 
-		return parser.parse(source, {
-			constructors	: ctors
-		});
-	}
+        if (!ctors.money) {
+            ctors.money = function(args) {
+                return new builtin.Money(args.major, args.minor, args.currency);
+            }
+        }
+
+        if (!ctors.vec2) {
+            ctors.vec2 = function(args) {
+                return new builtin.Vec2(args.x, args.y);
+            }
+        }
+
+        if (!ctors.vec3) {
+            ctors.vec3 = function(args) {
+                return new builtin.Vec3(args.x, args.y, args.z);
+            }
+        }
+
+        return parser.parse(source, {
+            constructors    : ctors
+        });
+    }
 };
